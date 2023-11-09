99f32704a2e67c4609787e4497a43e765a01183f
unknown <elmiwahyutriyanice@gmail.com>
2023-11-09 22:04:42 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,443 @@
+const green = "#AAD59E";
+const red = "#F0743E";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW6L1 = document.querySelector("#W6L1");
+
+  kolomW6L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "L") {
+      kolomW6L1.style.backgroundColor = green;
+    } else {
+      kolomW6L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW6L1 = document.querySelector("#W6L2");
+
+  kolomW6L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "O") {
+      kolomW6L1.style.backgroundColor = green;
+    } else {
+      kolomW6L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW6L1 = document.querySelector("#W6L4");
+
+  kolomW6L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "I") {
+      kolomW6L1.style.backgroundColor = green;
+    } else {
+      kolomW6L1.style.backgroundColor = red;
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW6L1 = document.querySelector("#W6L5");
+
+  kolomW6L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "N") {
+      kolomW6L1.style.backgroundColor = green;
+    } else {
+      kolomW6L1.style.backgroundColor = red;
+    }
+  });
+});
+/*==============*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW9L1 = document.querySelector("#W9L2");
+
+  kolomW9L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "I") {
+      kolomW9L1.style.backgroundColor = green;
+    } else {
+      kolomW9L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW9L1 = document.querySelector("#W9L3");
+
+  kolomW9L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "N") {
+      kolomW9L1.style.backgroundColor = green;
+    } else {
+      kolomW9L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW9L1 = document.querySelector("#W9L4");
+
+  kolomW9L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "K") {
+      kolomW9L1.style.backgroundColor = green;
+    } else {
+      kolomW9L1.style.backgroundColor = red;
+    }
+  });
+});
+
+/*=======5=======*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW5L1 = document.querySelector("#W5L1");
+
+  kolomW5L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "H") {
+      kolomW5L1.style.backgroundColor = green;
+    } else {
+      kolomW5L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW5L1 = document.querySelector("#W5L2");
+
+  kolomW5L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "T") {
+      kolomW5L1.style.backgroundColor = green;
+    } else {
+      kolomW5L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW5L1 = document.querySelector("#W5L3");
+
+  kolomW5L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "M") {
+      kolomW5L1.style.backgroundColor = green;
+    } else {
+      kolomW5L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW5L1 = document.querySelector("#W5L4");
+
+  kolomW5L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "L") {
+      kolomW5L1.style.backgroundColor = green;
+    } else {
+      kolomW5L1.style.backgroundColor = red;
+    }
+  });
+});
+
+/*=======4=======*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L1");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "B") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L2");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "R") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L3");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "O") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L4");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "W") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L5");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "S") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L6");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "E") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW4L1 = document.querySelector("#W4L7");
+
+  kolomW4L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "R") {
+      kolomW4L1.style.backgroundColor = green;
+    } else {
+      kolomW4L1.style.backgroundColor = red;
+    }
+  });
+});
+
+/*=======7=======*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW7L1 = document.querySelector("#W7L1");
+
+  kolomW7L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "C") {
+      kolomW7L1.style.backgroundColor = green;
+    } else {
+      kolomW7L1.style.backgroundColor = red;
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW7L1 = document.querySelector("#W7L3");
+
+  kolomW7L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "M") {
+      kolomW7L1.style.backgroundColor = green;
+    } else {
+      kolomW7L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW7L1 = document.querySelector("#W7L5");
+
+  kolomW7L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "E") {
+      kolomW7L1.style.backgroundColor = green;
+    } else {
+      kolomW7L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW7L1 = document.querySelector("#W7L7");
+
+  kolomW7L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "T") {
+      kolomW7L1.style.backgroundColor = green;
+    } else {
+      kolomW7L1.style.backgroundColor = red;
+    }
+  });
+});
+
+/*=======7=======*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW8L1 = document.querySelector("#W8L1");
+
+  kolomW8L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "U") {
+      kolomW8L1.style.backgroundColor = green;
+    } else {
+      kolomW8L1.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW8L1 = document.querySelector("#W8L3");
+
+  kolomW8L1.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "L") {
+      kolomW8L1.style.backgroundColor = green;
+    } else {
+      kolomW8L1.style.backgroundColor = red;
+    }
+  });
+});
+
+/*=======3=======*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW3L3 = document.querySelector("#W3L3");
+
+  kolomW3L3.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "R") {
+      kolomW3L3.style.backgroundColor = green;
+    } else {
+      kolomW3L3.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW3L3 = document.querySelector("#W3L4");
+
+  kolomW3L3.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "V") {
+      kolomW3L3.style.backgroundColor = green;
+    } else {
+      kolomW3L3.style.backgroundColor = red;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const kolomW3L3 = document.querySelector("#W3L5");
+
+  kolomW3L3.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value.toUpperCase();
+    if (nilaiInput === "E") {
+      kolomW3L3.style.backgroundColor = green;
+    } else {
+      kolomW3L3.style.backgroundColor = red;
+    }
+  });
+});
+let word1 = ["H", "T", "T", "P", "S"];
+let word2 = ["S", "E", "A", "R", "C", "H"];
+let word3 = ["S", "E", "R", "V", "E", "R"];
+let word4 = ["B", "R", "O", "W", "S", "E", "R"];
+let word5 = ["H", "T", "M", "L"];
+let word6 = ["L", "O", "G", "I", "N"];
+let word7 = ["C", "O", "M", "M", "E", "N", "T"];
+let word8 = ["U", "R", "L"];
+let word9 = ["L", "I", "N", "K"];
+let word10 = ["H", "O", "M", "E", "P", "A", "G", "E"];
+
+for (let i = 1; i <= word1.length; i++) {
+  const kolom = document.querySelector(`#W1L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word1[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
+
+for (let i = 2; i <= word2.length; i++) {
+  const kolom = document.querySelector(`#W2L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word2[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
+
+for (let i = 2; i <= word10.length; i++) {
+  const kolom = document.querySelector(`#W10L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word10[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
+
+for (let i = 1; i <= word3.length; i++) {
+  const kolom = document.querySelector(`#W3L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word3[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
+
+for (let i = 3; i <= word3.length; i++) {
+  const kolom = document.querySelector(`#W3L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word3[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
+for (let i = 3; i <= word10.length; i++) {
+  const kolom = document.querySelector(`#W3L${i}`);
+  kolom.addEventListener("input", (event) => {
+    let nilaiInput = event.target.value;
+    if (nilaiInput == word3[i - 1]) {
+      kolom.style.backgroundColor = green;
+    } else {
+      kolom.style.backgroundColor = red;
+    }
+  });
+}
